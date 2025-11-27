d1cdc736681ec4bba9e11b2338858190cf3a5487
Enriconi <gabriel.enriconi@gmail.com>
2025-11-27 02:23:00 -0300

    feat(introductory-cses): add cses1068

---
+++ cses1068.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define dbg(x) cout << #x << " = " << x << endl
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define x first
+#define y second
+#define mp make_pair
+#define pb push_back
+
+typedef long long ll;
+typedef pair<int, int> ii;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+int main() { _
+  ll n; cin >> n;
+  while (1) {
+    cout << n << ' ';
+    if (n == 1) break;
+    n = (n % 2 == 0) ? n/2 : 3*n+1;
+  }
+  cout << endl;
+  return 0;
+}
